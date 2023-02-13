7d3b504b564a8a476d1511f40efe9932d3d4b337
tariel36 <2964246+tariel36@users.noreply.github.com>
2023-02-13 12:31:35 +0900

    changes to product table

--- prj/frontend/src/stripe/stripe-product-table/stripe-product-table.component.ts
+++ prj/frontend/src/stripe/stripe-product-table/stripe-product-table.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { LocalStorageKey } from '../../local-storage/models/local-storage-key';
+import { ICustomer } from '../models/customer.interface';
 
 @Component({
   selector: 'app-stripe-product-table',
   templateUrl: './stripe-product-table.component.html',
   styleUrls: ['./stripe-product-table.component.scss']
 })
-export class StripeProductTableComponent {
+export class StripeProductTableComponent implements OnInit, AfterContentInit {
+  public isLoading = true;
 
+  public customerEmail = '';
+
+  constructor() {
+
+  }
+
+  public ngOnInit(): void {
+    this.customerEmail = (JSON.parse(localStorage.getItem(LocalStorageKey.Customer)) as ICustomer).email;
+    console.log('ngOnInit')
+  }
+
+  public ngAfterContentInit(): void {
+    this.isLoading = false;
+    console.log('ngAfterContentInit')
+  }
 }
