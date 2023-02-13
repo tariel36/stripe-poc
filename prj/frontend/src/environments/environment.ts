f787cb126ba5b7234a54f84e0bb231e327d331fe
tariel36 <2964246+tariel36@users.noreply.github.com>
2023-02-13 12:31:46 +0900

    lazy customer portal

--- prj/frontend/src/environments/environment.ts
+++ prj/frontend/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     currentUrl: 'http://localhost:4200',
     baseApiUrl: 'http://localhost:4300',
+    customerPortal: 'https://billing.stripe.com/p/login/test_aEU6oo6BZ4nego08ww'
 };
