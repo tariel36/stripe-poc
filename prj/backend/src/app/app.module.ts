cc15b036118a6b82ca7452999c1c7531c24e20e5
tariel36 <2964246+tariel36@users.noreply.github.com>
2023-02-14 10:21:13 +0900

    user module

--- prj/backend/src/app/app.module.ts
+++ prj/backend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { StripeModule } from '../stripe/stripe.module';
+import { UsersModule } from '../users/users.module';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 
@@ -10,6 +11,7 @@ import { AppService } from './services/app.service';
       isGlobal: true,
     }),
     StripeModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
