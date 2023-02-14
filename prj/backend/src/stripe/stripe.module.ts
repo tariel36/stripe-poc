e29677bde7539dd8794b6606df14e83705c4a8bd
tariel36 <2964246+tariel36@users.noreply.github.com>
2023-02-14 10:23:19 +0900

    import exports

--- prj/backend/src/stripe/stripe.module.ts
+++ prj/backend/src/stripe/stripe.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
 import { StripeController } from './controllers/stripe.controller';
 import { StripeService } from './services/stripe.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [StripeController],
   providers: [StripeService],
+  exports: [StripeService],
 })
 export class StripeModule {}
